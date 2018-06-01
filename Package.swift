e2c9340a4d4e560215c054713888bdf47b981c68
Erik Bean <erikbean@Eriks-MacBook-Pro.local>
2018-06-01 19:09:30 -0400

    Bug Fix
    Apple Bot now builds again!

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "AppleBot",
     dependencies: [
-        .package(url: "https://github.com/Azoy/Sword", from: "0.9.0")
+        .package(url: "https://github.com/Azoy/Sword", .branch("master")),
+        .package(url: "https://github.com/vapor-community/sockets", Version(2,2,3)...Version(2,2,3))
     ],
     targets: [
         .target(
